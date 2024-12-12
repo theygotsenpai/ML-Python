cbce0839b8bdccdabb298037d5bc1b5fda3cb977
Saurav Deka <143011580+theygotsenpai@users.noreply.github.com>
2024-12-12 16:00:03 +0530

    Update simple_banking_code.py
    Create a custom exception named `InsufficientBalanceError`. In the `BankAccount` class, raise this exception when a withdrawal amount is greater than the balance. Handle the exception and print an appropriate message.

--- simple_banking_code.py
+++ simple_banking_code.py
@@ -1,1 +1,24 @@
+class InsufficientBalance(Exception):
+  pass
+class BankAccount:
+  def __init__(self,account_number,balance):
+    self.account_number = account_number
+    self.__balance=balance
 
+  def deposit(self,amount):
+    self.__balance += amount
+
+  def withdraw(self,amount):
+    if amount > self.__balance:
+      raise InsufficientBalance("Insufficient Balance)
+    else:
+      self.__balance -= amount
+	
+	def check_balance(self):
+		return self.__balance
+account = BankAccount(77002123221,10000)
+account.deposit(50000)
+try:
+	account.withdraw(1000)
+except InsufficientBalance as e:
+	print(f"Error processing your request. \n {e}")
